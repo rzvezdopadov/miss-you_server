1bf872fc8ca95d67f573eac87867e06e9eee44c2
Roman Zvezdopadov <51131623+rzvezdopadov@users.noreply.github.com>
2024-03-04 21:33:10 +0300

    added console log guaard for query

---
+++ src/core/guards/testQuery.guard.ts
@@ -0,0 +1,15 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class TestQueryGuard implements CanActivate {
+    canActivate(
+        context: ExecutionContext,
+    ): boolean | Promise<boolean> | Observable<boolean> {
+        const request = context.switchToHttp();
+
+        console.log(request);
+
+        return true;
+    }
+}
